0d78edc5bed8c2681662978d7ea6b351049314b5
xxxibgridcoder23001 <thuyan.tran1103@gmail.com>
2025-05-19 23:42:02 +0700

    Update User.java

--- src/Model_class/User.java
+++ src/Model_class/User.java
@@ -40,6 +40,23 @@ public abstract class User {
         this("u_0000000000", "defaultUser", "defaultPassword", "01-01-2025_00:00:00", "customer");
     }
 
+    // Getters and setters
+    public String getUserId() { return userId; }
+    public void setUserId(String userId) { this.userId = userId; }
+
+    public String getUserName() { return userName; }
+    public void setUserName(String userName) { this.userName = userName; }
+
+    public String getUserPassword() { return userPassword; }
+    public void setUserPassword(String userPassword) { this.userPassword = userPassword; }
+
+    public String getUserRegisterTime() { return userRegisterTime; }
+    public void setUserRegisterTime(String userRegisterTime) { this.userRegisterTime = userRegisterTime; }
+
+    public String getUserRole() { return userRole; }
+    public void setUserRole(String userRole) { this.userRole = userRole; }
+
+
     /**
      * Encrypts the password using a simple encryption algorithm.
      * @param password The password to encrypt
